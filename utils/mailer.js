f9964ddd25dcce22c6022b10ae6227803b0d22e3
fusuke2823-gif <fusuke2823@gmail.com>
2026-03-18 23:40:58 +0900

    fix smtp: use port 587 with IPv4

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,8 +3,9 @@ const pool = require('../db/index');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
